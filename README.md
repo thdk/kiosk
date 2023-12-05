76001c0efda1347126b27cb9efc97aa50c744212
Thomas Dekiere <t.dekiere@gmail.com>
2023-12-05 11:05:10 +0100

    feat: add vite site

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# kiosk
+# thdk kiosk
+
---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import svelteLogo from './assets/limba-licht-op-2023.jpg'
+</script>
+
+<main>
+      <img src={svelteLogo} class="full-screen-image" alt="Limba Licht op 2023" />
+</main>
+
+
